cc75bfa70c6f07acb2b15b3d9f89b8492df568fc
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2022-09-05 18:30:49 +0700

    Add Assignment 2

---
+++ assignment-2/js/main.js
@@ -0,0 +1,23 @@
+
+
+
+function submit() {
+  let nama = document.getElementById("nama");
+  let role = document.getElementById("role");
+  let availability = document.getElementById("availability");
+  let age = document.getElementById("age");
+  let lokasi = document.getElementById("lokasi");
+  let experience = document.getElementById("experience");
+  let email = document.getElementById("email");
+
+  document.getElementById("data_nama").innerHTML = document.getElementById("nama").value
+  document.getElementById("data_role").innerHTML = role.value
+  document.getElementById("data_availability").innerHTML = availability.value
+  document.getElementById("data_age").innerHTML = age.value
+  document.getElementById("data_lokasi").innerHTML = lokasi.value
+  document.getElementById("data_experience").innerHTML = experience.value
+  document.getElementById("data_email").innerHTML = email.value
+
+  let form = document.getElementById("form");
+   form.className += "d-none";
+}
